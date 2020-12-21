f5ce3cd78316bc1ca836083aa5953d72e380b3cf
dev-promtheus98 <ozh98@protonmail.com>
2020-12-21 14:05:01 +0000

    custom pipe validating the task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,6 +12,7 @@ import {
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationpipe } from './pipes/task-status-validation.pipe';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -47,7 +48,7 @@ export class TasksController {
   @Patch('/:id/:status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Param('status') status: TaskStatus,
+    @Param('status', TaskStatusValidationpipe) status: TaskStatus,
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
